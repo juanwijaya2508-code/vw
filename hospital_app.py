951b41374da7b91b68cc6314319194dd7e43ce26
Juan Wijaya <juanwijaya2508@gmail.com>
2026-07-01 15:11:20 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -75,16 +75,36 @@ asthma = st.checkbox("Asthma")
 
 if st.button("Predict Departmen"):
    patient = pd.DataFrame([{
-     'age': age,
-      'gender' : gender_map.get(gender, 0),
-      'fever' : int(fever),
-      'cough' : int(cough),
-      'headache' : int(headache),
-      'chest_pain' : int(chest_pain),
-      'stomach_pain' : int(stomach_pain),
-      'shortness_of_breath' : int(shortness_of_breath),
-      'nausea_vomiting' : int(nausea_vomiting),
-      'dizziness': int(dizziness),
-      'skin_rash': int(skinrash),
+    'age': age,
+    'gender' : gender_map.get(gender, 0),
+     
+    'fever' : int(fever),
+    'cough' : int(cough),
+    'headache' : int(headache),
+    'chest_pain' : int(chest_pain),
+    'stomach_pain' : int(stomach_pain),
+    'shortness_of_breath' : int(shortness_of_breath),
+    'nausea_vomiting' : int(nausea_vomiting),
+    'dizziness': int(dizziness),
+    'skin_rash': int(skinrash),
 
-   }])
+
+'temperature_level':
+temp_map.get(temperature_level, 1)
+
+'heart_Rate_level':
+hr_map.get(heart_rate_level, 1,),
+
+'duration':
+dur_map.get(duration, 1),
+
+'asthma': int(asthma),
+'hypertension': int(hypertension),
+'heart_disease': int(heart_disease)
+
+'chief_complaint' :
+cc_map.get(chief_complaint, 9)
+
+  }])
+
+
